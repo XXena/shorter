5c5d3b002c5d6241633616ed0ea35de6e1489293
XXena <kvvolkova@gmail.com>
2022-08-14 17:37:06 +0300

    redirect handler refactoring

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -10,24 +10,22 @@ func (h *Handler) Redirect(w http.ResponseWriter, r *http.Request) {
 	shortURL := r.URL.RequestURI()
 	longURL, err := h.service.Redirect(shortURL)
 
-	if err != nil {
-		if strings.Contains(err.Error(), "no rows in result set") {
-			w.WriteHeader(http.StatusNotFound)
-			_, err = w.Write([]byte(fmt.Sprintf("Short link %s not found, create new one", shortURL)))
-			return
-		}
+	if err == nil {
+		http.Redirect(w, r, longURL, http.StatusFound)
 
-		h.logger.Error(fmt.Errorf("redirect failed: %w", err))
-		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte(fmt.Sprintf("internal error: %s", err.Error())))
-		if err != nil {
-			h.logger.Error(fmt.Errorf("internal error: %w", err))
-			return
-		}
 		return
 	}
 
-	http.Redirect(w, r, longURL, http.StatusFound)
+	if strings.Contains(err.Error(), "no rows in result set") {
+		w.WriteHeader(http.StatusNotFound)
+		_, err = w.Write([]byte(fmt.Sprintf("Short link %s not found, create new one", shortURL)))
+
+		return
+	}
+
+	h.logger.Error(fmt.Errorf("redirect failed: %w", err))
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+
 	return
 
 }
