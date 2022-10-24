5ca2b2bfabe71b5ede2e72609cbe8731c01b9ae0
XXena <kvvolkova@gmail.com>
2022-10-24 17:55:30 +0300

    cleanup comments

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,10 +4,6 @@ import (
 	"github.com/XXena/shorter/internal/entities"
 )
 
-//type Repository struct {
-//	RecordInterface
-//}
-
 type RecordInterface interface {
 	Create(record entities.Record) (id int, err error)
 	GetByURL(longURL string) (record entities.Record, err error)
@@ -15,10 +11,3 @@ type RecordInterface interface {
 	Update(recordID int, record entities.Record) error
 	Delete(recordID int) error
 }
-
-//
-//func NewRepository(db *pgx.Conn, l logger.Interface) RecordInterface {
-//	return &recordPostgres{
-//		RecordInterface: NewRecordPostgres(db, l),
-//	}
-//}
